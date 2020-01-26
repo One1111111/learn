6e6cd2e50c019b3f3ea3a04abc2649b121725bbf
lys <10021071@qq.com>
2020-01-26 21:56:09 +0800

    添加

---
+++ src/main/java/com/learn/studylearn/StudyLearnApplication.java
@@ -0,0 +1,13 @@
+package com.learn.studylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/studylearn/StudyLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.studylearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
